df25fe8fea5b767ab5adee758e8eb7543250b781
sumit sahni <3075sumitsahni@gmail.com>
2022-09-24 20:28:45 +0530

    update

--- realestate/src/Components/fetch.jsx
+++ realestate/src/Components/fetch.jsx
@@ -8,14 +8,12 @@ const Fetch= () => {
     const [item, setItem] = useState([]);
     
        useEffect(()=>{
-          fetch("http://localhost:3500/data")
-          .then(res =>{
-            return res.json();
-          })
-          .then(data =>{
-              console.log(data)
-              setItem(data)
-          })
+        const load = async() =>{
+          const devEnv = process.env.NODE_ENV !=="production";
+          const {REACT_AP} = process.env;
+          const response = await axios.get(`${devEnv ? REACT_AP:REACT_APP_DEV}`);
+        }
+       
        },[])
    
        
